03fbd7a46bb8e181bee6cc4b18d2e76a0ee4d9bd
Latasb78 <lata.24.beai@acharya.ac.in>
2025-05-24 18:54:00 +0530

    Add files via upload

---
+++ Zigzag.py
@@ -0,0 +1,24 @@
+import time
+import sys
+
+def zigzag():
+    indent = 0
+    indent_increasing = True
+
+    try:
+        while True:
+            print(' ' * indent + '*')
+            time.sleep(0.1)
+
+            if indent_increasing:
+                indent += 1
+                if indent == 20:
+                    indent_increasing = False
+            else:
+                indent -= 1
+                if indent == 0:
+                    indent_increasing = True
+    except KeyboardInterrupt:
+        sys.exit("\nZigzag animation stopped.")
+
+zigzag()
